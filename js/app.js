1a357de7a2adeb45d2ebaa412707a4d103589a50
Saeed0920 <sd.eed1381@gmail.com>
2023-04-28 23:17:01 +0430

    caculate basic function and fix some bugs

--- js/app.js
+++ js/app.js
@@ -1,38 +1,88 @@
 let vm = Vue.createApp({
   data() {
     return {
-      numbers: "0",
+      mainNumber: ``,
+      preNumber: ``,
+      preShow: ``,
+      operation: ``,
       rest: true,
       checkDot: false,
     };
   },
   methods: {
-    calculator() {},
     match(event) {
       if (this.rest) {
-        this.numbers = "";
+        this.mainNumber = ``;
+        this.preNumber = ``;
+        this.preShow = ``;
         this.rest = false;
       }
-      this.numbers += event.target.value;
+      this.mainNumber += event.target.textContent;
+    },
+    operationF(event) {
+      this.operation = event.target.value;
+      if (this.mainNumber === "") {
+        return;
+      } else if (this.preNumber !== "") {
+        this.calculator();
+      } else {
+        this.preNumber = this.mainNumber;
+        this.mainNumber += event.target.textContent;
+        this.preShow = this.mainNumber;
+        this.mainNumber = ``;
+      }
     },
     clear() {
-      this.numbers = "0";
+      this.mainNumber = ``;
+      this.preNumber = ``;
+      this.preShow = ``;
       this.rest = true;
       this.checkDot = false;
     },
     del() {
-      if (!this.rest && this.numbers.length > 1) {
-        this.numbers = this.numbers.substr(0, this.numbers.length - 1);
+      if (!this.rest && this.mainNumber.length > 1) {
+        this.mainNumber = this.mainNumber.substr(0, this.mainNumber.length - 1);
       }
     },
     dot() {
-      if (this.numbers.search(/[.]/g) === -1) {
+      if (this.mainNumber.search(/[.]/g) === -1) {
         this.checkDot = false;
       }
       if (!this.checkDot) {
-        this.numbers += ".";
+        this.mainNumber += ".";
         this.checkDot = true;
         this.rest = false;
+      }
+    },
+    calculator() {
+      const mainN = Number(this.mainNumber);
+      const preN = Number(this.preNumber);
+      let result;
+      console.log(preN, Number(this.preNumber));
+      console.log(mainN, Number(this.mainNumber));
+      if (isNaN(mainN) || isNaN(preN)) return;
+      switch (this.operation) {
+        case "x":
+          result = mainN * preN;
+          break;
+        case "/":
+          result = mainN / preN;
+          break;
+        case "-":
+          result = mainN - preN;
+          break;
+        case "+":
+          result = mainN + preN;
+          break;
+        case "%":
+          result = mainN % preN;
+          break;
+      }
+
+      if (result) {
+        this.preNumber += this.mainNumber;
+        this.preShow += this.mainNumber;
+        this.mainNumber = result;
       }
     },
   },
